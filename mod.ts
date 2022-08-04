706e9abd8a293c5cf17c10270f4320d56a6065b2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-05 01:55:35 +0900

    feat(mod.ts): export each message types

--- mod.ts
+++ mod.ts
@@ -1,7 +1,16 @@
 export {
+  type BidirectionalMessage,
   type ClientMessage,
+  type CompleteMessage,
+  type ConnectionAckMessage,
+  type ConnectionInitMessage,
+  type ErrorMessage,
   type Message,
+  type NextMessage,
+  type PingMessage,
+  type PongMessage,
   type ServerMessage,
+  type SubscribeMessage,
 } from "./message.ts";
 export { default as MessageType } from "./message_type.ts";
 export { PROTOCOL } from "./constants.ts";
