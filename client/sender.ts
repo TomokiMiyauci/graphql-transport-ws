2e6cf45a9467e1da22216e268491657969a7899b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-05 16:20:49 +0900

    feat(client): use lazy sending message

--- client/sender.ts
+++ client/sender.ts
@@ -1,6 +1,5 @@
 import { Sender, SenderImpl } from "../sender.ts";
 import { ClientMessenger } from "./message.ts";
-import { safeSend } from "../utils.ts";
 import { GraphQLParameters } from "../deps.ts";
 
 export interface ClientSender extends Sender {
@@ -13,15 +12,50 @@ export class ClientSenderImpl extends SenderImpl implements ClientSender {
     super(socket);
   }
 
-  connectionInit(): void {
-    safeSend(this.socket, ClientMessenger.connectionInit());
+  connectionInit() {
+    lazySend(
+      this.socket,
+      JSON.stringify(ClientMessenger.connectionInit()),
+    );
   }
 
   subscribe(id: string, payload: GraphQLParameters): void {
-    safeSend(this.socket, ClientMessenger.subscribe(id, payload));
+    lazySend(
+      this.socket,
+      JSON.stringify(ClientMessenger.subscribe(id, payload)),
+    );
   }
 }
 
 export function createSender(socket: WebSocket): ClientSender {
   return new ClientSenderImpl(socket);
+}
+
+type Result = {
+  /** Whether send message immediately or not.　*/
+  immediate: boolean;
+  dispose?: () => void;
+};
+
+function lazySend(
+  socket: WebSocket,
+  data: string | ArrayBufferLike | Blob | ArrayBufferView,
+): Result {
+  function openHandler(): void {
+    socket.send(data);
+  }
+
+  if (socket.readyState === socket.OPEN) {
+    socket.send(data);
+    return { immediate: true };
+  }
+
+  socket.addEventListener("open", openHandler, { once: true });
+  const dispose = (): void => {
+    socket.removeEventListener("open", openHandler);
+  };
+
+  socket.addEventListener("close", dispose, { once: true });
+
+  return { immediate: false, dispose };
 }
